a8b888b3862f0a16bfb0ebd5b0c4fab93bb9883b
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-09-18 22:19:39 +0300

    4. Add Redux Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
 //context providers
 import AuthProvider from "./contexts/authContext";
-
+import store from "./components/redux/reducer/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <AuthProvider>
+    <Provider store={store}>
       <App />
+    </Provider>
     </AuthProvider>
   </Router>
 );
