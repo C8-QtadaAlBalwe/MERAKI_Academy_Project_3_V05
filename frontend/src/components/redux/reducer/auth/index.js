c9c6e97b809338dd07051531a1cac3239c7a0384
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-09-18 21:15:47 +0300

    1. Create Auth Reducer

---
+++ frontend/src/components/redux/reducer/auth/index.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const auth =createSlice({
+    name: "auth",
+    initialState:{
+        token:localStorage.getItem("token") || null,
+        userId : localStorage.getItem("userId") || null,
+        isLoggedIn: localStorage.getItem("token")? true:false
+    },
+    reducers:{
+    setLogin :(state,action)=>{
+    state.token=action.payload
+    state.isLoggedIn=true,
+    localStorage.setItem("token",state.token)
+    },
+    setUserId :(state ,action)=>{
+    state.userId=action.payload,
+    localStorage.setItem("userId",state.userId)
+    },
+    setLogout :(state,action)=>{
+    state.token = null,
+    state.userId = null,
+    state.isLoggedIn=false,
+    localStorage.clear();
+    }
+    }
+
+});
+export const {setLogin,setLogout}=counterSlice.actions
+export default counterSlice.reducer
